4f6bb4fb641b82a929428f3834b6b0197966878e
Irene Rubio <rubio.hernandez.irene@gmail.com>
2025-09-08 13:54:28 +0200

    last

--- src/app/components/RecipeCard.tsx
+++ src/app/components/RecipeCard.tsx
@@ -65,7 +65,7 @@ export default function RecipeCard() {
               className="flex-1 text-stone-400"
             />
 
-            <button className="bg-cyan-800 text-white font-semibold px-6 py-2 rounded-lg hover:bg-cyan-600 transition">
+            <button className="bg-black text-white font-semibold px-6 py-2 rounded-lg hover:bg-fuchsia-700 transition">
               Generate
             </button>
           </div>
