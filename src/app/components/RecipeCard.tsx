0e9a812fec9c83345dda3c54dd3203992de2c22f
Irene Rubio <rubio.hernandez.irene@gmail.com>
2025-09-09 13:02:05 +0200

    new commit

--- src/app/components/RecipeCard.tsx
+++ src/app/components/RecipeCard.tsx
@@ -1,11 +1,8 @@
 "use client";
 
-import { useState } from "react";
 import { Recipe } from "@/types";
 import dummyRecipe from "@/data/dummyRecipe.json";
-import { Input } from "./iu/input";
-import { Select } from "./iu/select";
-import { TextGif } from "./iu/text-gif";
+
 import { isRecipe } from "@/types/validation";
 
 if (!isRecipe(dummyRecipe)) {
@@ -15,105 +12,49 @@ if (!isRecipe(dummyRecipe)) {
 const recipe: Recipe = dummyRecipe;
 
 export default function RecipeCard() {
-  const [foodType, setFoodType] = useState("");
-  const [selectedOption, setSelectedOption] = useState("");
-
-  const selectOptions = [
-    { label: "Select your level", value: "" },
-    { label: "I am a beguinner", value: "easy" },
-    { label: "I am intermidate", value: "medium" },
-    { label: "I am advanced", value: "hard" },
-  ];
-  const gifUrls = [
-    "https://media.giphy.com/media/3zvbrvbRe7wxBofOBI/giphy.gif",
-    "https://media.giphy.com/media/fnglNFjBGiyAFtm6ke/giphy.gif",
-  ];
   return (
     <div className="relative min-h-screen">
-      <div className="absolute inset-0">
-        <img
-          src="/varios-vegetales-.jpg"
-          alt="background"
-          className="w-full h-full object-cover opacity-80"
-        />
-      </div>
-
-      <div className="relative z-10 flex flex-col items-center p-10 space-y-10">
-        <div className="w-full max-w-3xl bg-white shadow-lg rounded-2xl p-8 mt-10 text-center">
-          <TextGif
-            gifUrl={gifUrls[0]}
-            text="Foody Goody"
-            size="xxl"
-            weight="medium"
+      <div className="absolute inset-0"></div>
+      <div className="w-full max-w-3xl bg-white shadow-lg rounded-2xl overflow-hidden border border-gray-200">
+        <div className="p-6">
+          <h1 className="text-3xl font-bold mb-2 text-gray-800">
+            {recipe.name}
+          </h1>
+          <p className="pb-4">{recipe.description}</p>
+          <img
+            src="/sushi.avif"
+            alt={recipe.name}
+            className="w-full h-80 object-cover object-center rounded-lg mb-4"
           />
-          <p className="text-2xl font-bold text-orange-400 opacity-70 pb-4">
-            your recipe generator
+
+          <h2 className="mt-6 text-2xl font-bold mb-2 text-gray-800">
+            Ingredients
+          </h2>
+          <ul className="list-disc pl-5">
+            {recipe.ingredients.map((ing, idx) => (
+              <li key={idx}>{ing}</li>
+            ))}
+          </ul>
+
+          <h2 className="mt-6 text-2xl font-bold mb-2 text-gray-800">
+            Cooking time
+          </h2>
+          <p>{recipe.cookTime} min</p>
+
+          <h2 className="mt-6 text-2xl font-bold mb-2 text-gray-800">
+            Preparation Time
+          </h2>
+          <p>{recipe.prepTime} min</p>
+
+          <h2 className="mt-6 text-2xl font-bold mb-2 text-gray-800">
+            Nutrition
+          </h2>
+          <p>
+            Calories: {recipe.nutrition.calories} <br />
+            Carbs: {recipe.nutrition.carbs} <br />
+            Fat: {recipe.nutrition.fat} <br />
+            Protein: {recipe.nutrition.protein}
           </p>
-
-          <div className="flex flex-col md:flex-row gap-4 items-center justify-center">
-            <Input
-              value={foodType}
-              onChange={(e) => setFoodType(e.target.value)}
-              placeholder="Type of food (e.g. sushi, pasta...)"
-              className="flex-1"
-            />
-
-            <Select
-              options={selectOptions}
-              value={selectedOption}
-              onChange={(e) => setSelectedOption(e.target.value)}
-              className="flex-1 text-stone-400"
-            />
-
-            <button className="bg-black text-white font-semibold px-6 py-2 rounded-lg hover:bg-fuchsia-700 transition">
-              Generate
-            </button>
-          </div>
-
-          <div className="mt-6"></div>
-        </div>
-
-        <div className="w-full max-w-3xl bg-white shadow-lg rounded-2xl overflow-hidden border border-gray-200">
-          <div className="p-6">
-            <h1 className="text-3xl font-bold mb-2 text-gray-800">
-              {recipe.name}
-            </h1>
-            <p className="pb-4">{recipe.description}</p>
-            <img
-              src="/otro-sushi.avif"
-              alt={recipe.name}
-              className="w-full h-80 object-cover object-center rounded-lg mb-4"
-            />
-
-            <h2 className="mt-6 text-2xl font-bold mb-2 text-gray-800">
-              Ingredients
-            </h2>
-            <ul className="list-disc pl-5">
-              {recipe.ingredients.map((ing, idx) => (
-                <li key={idx}>{ing}</li>
-              ))}
-            </ul>
-
-            <h2 className="mt-6 text-2xl font-bold mb-2 text-gray-800">
-              Cooking time
-            </h2>
-            <p>{recipe.cookTime} min</p>
-
-            <h2 className="mt-6 text-2xl font-bold mb-2 text-gray-800">
-              Preparation Time
-            </h2>
-            <p>{recipe.prepTime} min</p>
-
-            <h2 className="mt-6 text-2xl font-bold mb-2 text-gray-800">
-              Nutrition
-            </h2>
-            <p>
-              Calories: {recipe.nutrition.calories} <br />
-              Carbs: {recipe.nutrition.carbs} <br />
-              Fat: {recipe.nutrition.fat} <br />
-              Protein: {recipe.nutrition.protein}
-            </p>
-          </div>
         </div>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,76 @@
-import Image from "next/image";
+"use client";
+
 import RecipeCard from "./components/RecipeCard";
 
+import { useState } from "react";
+import { Recipe } from "@/types";
+import dummyRecipe from "@/data/dummyRecipe.json";
+import { Input } from "./components/iu/input";
+import { Select } from "./components/iu/select";
+import { TextGif } from "./components/iu/text-gif";
+import { isRecipe } from "@/types/validation";
+
 export default function Home() {
+  const [foodType, setFoodType] = useState("");
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const selectOptions = [
+    { label: "Select your level", value: "" },
+    { label: "I am a beginner", value: "easy" },
+    { label: "I am intermediate", value: "medium" },
+    { label: "I am advanced", value: "hard" },
+  ];
+  const gifUrls = [
+    "https://media.giphy.com/media/3zvbrvbRe7wxBofOBI/giphy.gif",
+    "https://media.giphy.com/media/fnglNFjBGiyAFtm6ke/giphy.gif",
+  ];
   return (
-    <div>
-      <RecipeCard />
+    <div className="relative min-h-screen">
+      <div className="absolute inset-0">
+        <img
+          src="/vegetables-.jpg"
+          alt="background"
+          className="w-full h-full object-cover opacity-80"
+        />
+      </div>
+
+      <div className="relative z-10 flex flex-col items-center p-10 space-y-10">
+        <div className="w-full max-w-3xl bg-white shadow-lg rounded-2xl p-8 mt-10 text-center">
+          <TextGif
+            className="p-1"
+            gifUrl={gifUrls[0]}
+            text="Foody Goody"
+            size="xxl"
+            weight="medium"
+          />
+          <p className="text-2xl font-bold text-orange-400 opacity-70 pb-4">
+            Your Recipe Generator
+          </p>
+
+          <div className="flex flex-col md:flex-row gap-4 items-center justify-center">
+            <Input
+              value={foodType}
+              onChange={(e) => setFoodType(e.target.value)}
+              placeholder="Type of food (e.g. sushi, pasta...)"
+              className="flex-1"
+            />
+
+            <Select
+              options={selectOptions}
+              value={selectedOption}
+              onChange={(e) => setSelectedOption(e.target.value)}
+              className="flex-1 text-stone-400"
+            />
+
+            <button className="bg-black text-white font-semibold px-6 py-2 rounded-lg hover:bg-fuchsia-700 transition">
+              Generate
+            </button>
+          </div>
+
+          <div className="mt-6"></div>
+        </div>
+        <RecipeCard />
+      </div>
     </div>
   );
 }
